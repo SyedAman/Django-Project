e194c479c8bf3595d7c44683d4f1547dc5dc4321
Syed Aman <sna274@nyu.edu>
2016-12-13 20:15:39 -0500

    point root URLconf at polls.url URLconf

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # point root URLconf at polls.url URLconf
     url(r'^polls/', include('polls.urls')),
 ]
