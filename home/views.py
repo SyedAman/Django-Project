abdfe2a6cd67f3c8aa1faa80f5ee01082a78167d
Syed Aman <sna274@nyu.edu>
2016-12-27 21:45:49 -0500

    add button to direct to polls view

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(response):
-    return HttpResponse('<h1>Home</h1>')
+    return HttpResponse('<h1>Home</h1><a href="/polls"><button>Polls</button></a>')
